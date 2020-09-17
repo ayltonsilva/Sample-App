602f1f8a38c319bfc3a582f38ef83eeba9df9549
Aylton Silva <runtime@Runtimes-MBP.Home>
2020-09-17 14:30:49 +0100

    Finish user signup

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   def setup
     @user = User.new(name: "Example User", email: "user@example.com",
-      password: "foobar", password_confirmation: "foobar")
+      password: "foobar", password_confirmation: "foobar", phone_number: "975335062")
   end
 
   test "should be valid" do 
@@ -58,6 +58,11 @@ class UserTest < ActiveSupport::TestCase
   
   test "password should have a minimum length" do 
     @user.password = @user.password_confirmation = "a" * 5 
+    assert_not @user.valid?
+  end
+
+  test "phone number should have a minimum length" do 
+    @user.phone_number = "a" * 8 
     assert_not @user.valid?
   end
 end
