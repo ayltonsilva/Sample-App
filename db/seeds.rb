20b21956ffd7d50c2f7bbf129561eff58bc36e4a
Aylton Silva <runtime@Runtimes-MBP.Home>
2020-09-18 12:40:44 +0100

    Finish user edit, update, index and destroy actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create a main sample user.
+User.create!(name:  "Example User",
+            email: "example@railstutorial.org",
+            password: "foobar",
+            password_confirmation: "foobar", 
+            phone_number: "999999999",
+            admin: true)
+
+    
+
+# Generate a bunch of additional users.
+99.times do |n|
+    name = Faker::Name.name
+    email = "example-#{n+1}@railstutorial.org" 
+    password = "password"
+    phone_number = "99999999#{n+1}"
+    User.create!(name: name,
+    email: email,
+    password: password,
+    password_confirmation: password,
+    phone_number: phone_number)
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,3 +15,12 @@ class ActiveSupport::TestCase
   end
   # Add more helper methods to be used by all tests here...
 end
+
+class ActionDispatch::IntegrationTest
+
+  # Log in as a particular user.
+  def log_in_as(user, password: 'password') 
+    post login_path, params: { session: { email: user.email,
+      password: password } }
+  end
+end
